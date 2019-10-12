e5164a4ca52eed0d63f0c893d1b78bdef5885aef
Jose Gutierrez <josegtzr99@gmail.com>
2019-10-12 13:16:50 -0400

    MVVM completo

---
+++ mvcPhone/appPhone/appPhone/Interfaces/InstanceLocator.cs
@@ -0,0 +1,19 @@
+﻿namespace appPhone.Interfaces
+{
+    using ViewModels;
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion      
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+
+
+
+}
